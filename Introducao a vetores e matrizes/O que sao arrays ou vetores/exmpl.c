7ce6a9c89fa610aace7b39c6ef91a2d089dd5b9a
Your Name <nunesikaro@gmail.com>
2025-10-30 07:54:47 -0300

    first commit

---
+++ Introducao a vetores e matrizes/O que sao arrays ou vetores/exmpl.c
@@ -0,0 +1,12 @@
+//Em programação, um array é uma estrutura de dados que armazena uma coleção de elementos do mesmo tipo em locais de memória contíguos (lado a lado). Arrays podem ter uma ou mais dimensões. Um array unidimensional é chamado de vetor. 
+
+//Um vetor, portanto, é uma sequência ordenada de elementos, onde cada elemento pode ser acessado diretamente por sua posição na sequência, chamada de índice.
+
+//Uma característica importante dos arrays (e, consequentemente, dos vetores em C) é que o primeiro elemento sempre está localizado no índice zero. Por exemplo, um array de inteiros com cinco posições terá índices de 0 a 4.
+
+  int numeros[5] = {10, 20, 30, 40, 50};
+    // Aqui, 'numeros' é um array (ou vetor) de inteiros com 5 elementos.
+
+//Neste exemplo, numeros é um vetor que armazena cinco números inteiros. Para acessar o primeiro elemento (o número 10), usaríamos numeros[0]. Para acessar o terceiro elemento (o número 30), usaríamos numeros[2], e assim por diante.
+
+
---
+++ Introducao a vetores e matrizes/O que sao matrizes/exmplM.c
@@ -0,0 +1,15 @@
+//Uma matriz é um array multidimensional. A matriz mais comum é a bidimensional, que pode ser visualizada como uma tabela organizada em linhas e colunas. Assim como nos vetores, cada elemento em uma matriz pode ser acessado por seus índices, mas agora precisamos de dois índices: um para a linha e outro para a coluna.
+
+     int tabela[3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+//Neste exemplo, tabela é uma matriz 3x3. Para acessar o elemento na primeira linha e segunda coluna (o número 2), usaríamos tabela[0][1]. O elemento na terceira linha e terceira coluna (o número 9) seria acessado com tabela[2][2]. A capacidade de organizar dados em múltiplas dimensões torna as matrizes ferramentas poderosas para representar informações complexas, como imagens, tabelas e grafos.
+
+//A diferença entre vetor e matrizes pode ser resumida em:
+
+//Vetor é unidimensional. 
+//Matriz é multidimensional (duas ou mais dimensões).
+
