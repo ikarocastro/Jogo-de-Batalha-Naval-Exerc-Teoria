8a348e5b9b2bb24c98017c1be8fe981c3988b5c5
Your Name <nunesikaro@gmail.com>
2025-10-31 11:49:18 -0300

    adicionei e estudei matrizes

---
+++ Matrizes em C/O que sao matrizes/exmplMA.c
@@ -0,0 +1,13 @@
+//São uma generalização dos arrays. Enquanto um array é unidimensional, uma matriz pode ter duas ou mais dimensões. A matriz bidimensional, a mais comum, é essencialmente uma tabela ou grade de valores em que cada elemento é acessado por dois índices: um para a linha e outro para a coluna.
+
+//========= O que são matrizes? ==========
+
+//Tambem conhecidas comoArray multidimensionais
+//Contem linhas e colunas
+//Forma mais comumm de utilização é a matriz bidimensional
+//Multiplos indices para acessar os elementos
+   //Linha
+   //Coluna
+
+
+    
---
+++ Matrizes em C/inicializacao simplificada/exmplIS.c
@@ -0,0 +1,8 @@
+int matriz[3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+//Semelhante aos vetores, você pode inicializar uma matriz ao mesmo tempo em que a declara
+
